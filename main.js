95776ed6e3e4079be5db9316132f0bf20367efe1
EC2 Default User <ec2-user@ip-172-31-29-186.us-east-2.compute.internal>
2021-08-31 10:52:16 +0000

    バグ修正 1.四則演算の連続入力不可 2.少数点、0の入力制御追加

--- main.js
+++ main.js
@@ -1,15 +1,95 @@
 let result = document.getElementById('textResult');
 
+
 function calcButtonClick(elem){
-  if(elem.value=='='){   
-    result.value = Function('return ('+result.value+');')();
+  let resultRightValue = (result.value.slice(-1)); //一番右の文字
+  let resultValueLength = (result.value).length; //文字数
+
+  if(elem.value=='='){  
+    if(resultRightValue=='+'||resultRightValue=='-'||resultRightValue=='*'||resultRightValue=='/'){
+    }else{
+      result.value = Function('return ('+result.value+');')();
+    }
   }else if(elem.value=='ac'){
     result.value = 0;
-  }else{
+  }else if(elem.value=='.'){
+    if(resultRightValue=='+'||resultRightValue=='-'||resultRightValue=='*'||resultRightValue=='/'){
+    }else if(hanteiDecimalPoint(result.value)=='OK'){
+      result.value = result.value + elem.value;
+    }else if(hanteiDecimalPoint(result.value)=='NG'){
+    }
+  }else if(elem.value=='+'||elem.value=='-'||elem.value=='*'||elem.value=='/'){
     if(result.value==0){
+    }else if(resultRightValue=='+'||resultRightValue=='-'||resultRightValue=='*'||resultRightValue=='/'){
+      result.value = result.value.slice(0,resultValueLength-1) + elem.value; //一番右の文字以外+入力文字
+    }else if(resultRightValue=='.'){
+    }
+    else{
+      result.value = result.value + elem.value;
+    }
+  }else if(elem.value=='00'){
+    if(result.value=='0'){
+    }
+    else if(resultRightValue=='+'||resultRightValue=='-'||resultRightValue=='*'||resultRightValue=='/'){
+    }
+    else if(resultRightValue=='0'){
+      if(hantei0(result.value)=='OK'){
+        result.value = result.value + elem.value;
+      }else if(hantei0(result.value)=='NG'){
+      }
+    }
+    else{
+      result.value = result.value + elem.value;
+    }
+  }else{
+    if(resultRightValue == '.'){
+      result.value = result.value + elem.value;
+    }else if(resultRightValue==0){
+      if(resultValueLength==1){
         result.value = elem.value;
+      }else{
+        if(hantei0(result.value)=='OK'){
+          result.value = result.value + elem.value;
+        }else if(hantei0(result.value)=='NG'){
+        }
+      }
     }else{
       result.value = result.value + elem.value;
     }
   }
+}
+
+
+function hanteiDecimalPoint(suusiki){
+  //数式を受け取り、小数点が入力出来るか判定する。
+  //OK：入力可、NG：入力不可。
+  let suusikiSplit = suusiki.split('');
+  let hantei = 'OK'
+
+  for(let i= suusikiSplit.length; i>=0;i--){
+    if(suusikiSplit[i]=='.'){
+        hantei = 'NG';
+        break;
+    }else if(suusikiSplit[i]=='+'||suusikiSplit[i]=='-'||suusikiSplit[i]=='*'||suusikiSplit[i]=='/'){
+      hantei = 'OK';
+      break;
+    }
+  }
+  return hantei;
+}
+
+
+function hantei0(suusiki){
+  //数式を受け取り、  0が入力出来るか判定する。
+  //OK：入力可、NG：入力不可。
+  let suusikiSplit = suusiki.split('');
+  let yousoNo = suusikiSplit.length-2;
+  let hantei = 'OK'
+
+  if(suusikiSplit[yousoNo]=='+'||suusikiSplit[yousoNo]=='-'||suusikiSplit[yousoNo]=='*'||suusikiSplit[yousoNo]=='/'){
+      hantei = 'NG';
+  }else{
+    hantei = 'OK';
+  }
+  return hantei;
 }
